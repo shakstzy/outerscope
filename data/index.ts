60cf422c5689da87a9dd6721c050c49831f6ec83
Adithya Kumar <shakstzy@Adithyas-MacBook-Pro.local>
2024-09-29 14:14:55 -0700

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,161 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "We relentlessly prioritize getting things done",
+    description: "Results-oriented execution",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexiblity with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "our network",
+    description: "Constantly scaling",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "for competitive verticals",
+    description: "Creative Solutions",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "with a passion for growth",
+    description: "Web3 Engineers",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Want to get in touch?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Layer",
+    des: "Build full stack decentralized protocols",
+    img: "/layer.png",
+    iconLists: [],
+    link: "layer.xyz",
+    actuallink:"https://www.layer.xyz/",
+  },
+  {
+    id: 2,
+    title: "Octane",
+    des: "Developer-first platform to battle-test your smart contracts with AI.",
+    img: "/octane.png",
+    iconLists: [],
+    link: "octane.security",
+    actuallink:"https://www.octane.security/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Not only did he excel in technical aspects, but Adithya also showcased a strong commitment to teamwork and collaboration. His proactive approach and willingness to go the extra mile greatly benefited beyond just the Retail team and impacted other parts of the business. During our collaboration, Adithya consistently demonstrated a strong work ethic, attention to detail, and a passion for problem-solving. His contributions played a crucial role in the successful implementation of analytics and dashboards for the Retail Partnerships team.",
+    name: "Pothana Kambhapati",
+    title: "Data/Development Lead @ SpaceX",
+  },
+  {
+    quote:
+      "[Adithya] made formative changes improving the internal workflows and resources that still remain active today. I am glad to have met Adi in this capacity and attest to his character, positive get-it-done attitude, and work ethic that will enable him wherever his pursuits take him.",
+    name: "Elan Okonsky",
+    title: "Starlink @ SpaceX",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "layer",
+    img: "/layer.svg",
+    nameImg: "/layerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "SpaceX Product Engineer",
+    desc: "Profit optimization, data analysis, cost planning & projection, internal tooling for Starlink/Starship engineering & supply chain for $250M+ ARR team",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Stanford & UC Berkeley",
+    desc: "EECS + Business @ UC Berkeley MET Program Business Administration @ Stanford University",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 3,
+    title: "Web3 Consultant",
+    desc: "Prev @ Paypal Crypto, Dfinity, Monte Carlo, Stake BTC, built internal DeFi tooling & DAO, closed 1B+ TVL & 300+ strategic partnerships in 8 months for clients",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Web2 Software/Operations",
+    desc: "Prev @ Cisco, NetApp, SoFi, Instagram, Dashlabs, identifed & drove $50M+ in cross-vertical revenue opps, shipped tooling/products for $2B+ divisions",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/three.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
