7302997324d62e1f5ea6308676d2b2a22eabb872
Adithya Kumar <shakstzy@Adithyas-MacBook-Pro.local>
2024-09-29 23:34:19 -0400

    fixed image type issue

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -14,6 +14,7 @@ export const InfiniteMovingCards = ({
     quote: string;
     name: string;
     title: string;
+    image: string;
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
